4c781f231621fab7dbc445c0367f3077f205c090
naikymen <naikymen@qb.fcen.uba.ar>
2014-04-03 21:54:41 -0300

    le code

---
+++ codeContador/contador.py
@@ -0,0 +1,14 @@
+__author__ = 'naikymen'
+
+ptmlistfile = open('ptmlist.txt', 'r')
+#print ptmlistfile.readline()
+ptmlistid = []
+i = 1
+for line in ptmlistfile:
+	#print line
+	esta = line.count('ID   ')
+	if esta == 1:
+		print('ocurrencia ',i)
+		i = i+1
+
+ptmlistfile.close()
