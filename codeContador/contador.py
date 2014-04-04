f8c01cffa1b45a9d32beb20e97dbebd8118c96a5
naikymen <naikymen@users.noreply.github.com>
2014-04-03 23:27:25 -0300

    +Comentarios

--- codeContador/contador.py
+++ codeContador/contador.py
@@ -1,13 +1,17 @@
 __author__ = 'naikymen'
 
+#abrir el archivo que contiene la lista de modificaciones postraduccionales de uniprot
 ptmlist = open('../ptmlist', 'r')
-#print ptmlistfile.readline()
+
+#contenedor para las ptm's
 ptmlistid = []
-i = 1
+
+#buscar lineas que contengan el ID de la ptm y concatenarlas a la lista ptmlistid
 for line in ptmlist:
-    #print line
     lineadeID = line.count('ID   ')
     if lineadeID == 1:
         ptmlistid.append(line)
+
+#imprimir dos los primeros dos elementos porque sí
 print(ptmlistid[:2])
 ptmlist.close()
