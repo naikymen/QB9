3d3deed358584d6626893a366d0435507750ebba
naikymen <naikymen@qb.fcen.uba.ar>
2014-04-04 02:19:15 -0300

    contador por ID completo

--- codeContador/contador.py
+++ codeContador/contador.py
@@ -5,13 +5,30 @@ ptmlist = open('../ptmlist', 'r')
 
 #contenedor para las ptm's
 ptmlistid = []
+ptmlistdupes = []
+print(ptmlistid)
 
-#buscar lineas que contengan el ID de la ptm y concatenarlas a la lista ptmlistid
+#itera en las lienas y en cada una las courrencias de 'ID   '
 for line in ptmlist:
     lineadeID = line.count('ID   ')
+    line = line.rstrip('\n')  # quita caracter de nueva linea a la derecha
+    line = line.lstrip('ID   ')  # quita segmento 'ID   ' a la izquierda
     if lineadeID == 1:
-        ptmlistid.append(line)
+        if line not in ptmlistid:  # si la linea es de ID y no está en la lista, agregarla a la lista
+            #print(line)
+            ptmlistid.append(line)
+        else:
+            ptmlistdupes.append(line)
 
-#imprimir dos los primeros dos elementos porque sí
-print(ptmlistid[:2])
+#imprimir la lista en líneas
+for item in ptmlistid:
+    print(item)
+
+#contar elementos en la lista
+print('Existen', len(ptmlistid), 'tipos de residuos modificados')
+print(len(ptmlistdupes), 'son duplicados según el ID')
+
+
+
+#cerrar el archivo
 ptmlist.close()
