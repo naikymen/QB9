6022475a4d812fa3cf2f3af663d00f551a13dd40
naikymen <naikymen@qb.fcen.uba.ar>
2014-04-16 22:05:11 -0300

    postgrecito

--- examples/tests.py
+++ examples/tests.py
@@ -1,43 +1,9 @@
+# coding=utf-8
 __author__ = 'nicolas'
+ptmlist = open('../ptmlist', 'r')
 
-dummy = 'dummy'
+line = ptmlist.readline()
 
-record = {
-    'ID': dummy,
-    'AC': dummy,
-    'FT': dummy,
-    'TG': dummy,
-    'PA': dummy,
-    'PP': dummy,
-    'CF': dummy,
-    'MM': dummy,
-    'MA': dummy,
-    'LC': dummy,
-    'TR': dummy,
-    'KW': dummy,
-    'DR': dummy,
-}
-
-categories = (
-    'ID',
-    'AC',
-    'FT',
-    'TG',
-    'PA',
-    'PP',
-    'CF',
-    'MM',
-    'MA',
-    'LC',
-    'TR',
-    'KW',
-    'DR',
-)
-
-print categories[0]
-print record
-
-#linecat = "hola"
-#print linecat[:99]
-#print range(0, 10)
-print(len(categories))
+while line:
+    line = ptmlist.readline()
+    print line
--- insertorr/ptminsertorr.py
+++ insertorr/ptminsertorr.py
@@ -1,14 +1,39 @@
 # coding=utf-8
 __author__ = 'nicolas'
+from peewee import *
+import psycopg2
 
 ptmlist = open('../ptmlist', 'r')
 
-categories = {
-}
+DBNAME = 'ptm_db'
+usuario = 'usuariocopado784'
+ptm_db = PostgresqlDatabase(DBNAME, user=usuario)
+ptm_db.get_conn().set_client_encoding('UTF8')
 
-dummy = ''
 
-categories = (
+class PostgresqlModel(Model):
+    ID = TextField(unique=True, primary_key=True, help_text='Identifier (FT description); starts a PTM entry')
+    AC = TextField(unique=True, help_text='Accession (PTM-xxxx)')
+    FT = TextField(help_text='Feature Key')
+    TG =
+    PA
+    PP
+    CF
+    MM
+    MA
+    LC
+    TR
+    KW
+    DR
+
+
+    class Meta:
+        database = ptm_db
+
+dummy = ''  # valor por defecto de los campos del registro
+record_cero = {}  # un diccionario (el registro) que va a contener los datos para una PTM
+
+categories = {  # lista del conjunto de categorías posibles
     'ID',
     'AC',
     'FT',
@@ -22,51 +47,34 @@ categories = (
     'TR',
     'KW',
     'DR',
-)
-
-record_cero = {
-    'ID': dummy,
-    'AC': dummy,
-    'FT': dummy,
-    'TG': dummy,
-    'PA': dummy,
-    'PP': dummy,
-    'CF': dummy,
-    'MM': dummy,
-    'MA': dummy,
-    'LC': dummy,
-    'TR': dummy,
-    'KW': dummy,
-    'DR': dummy,
 }
-record = record_cero
-"""
-for line in ptmlist:
-    recordno = 1
-    linecat = line[:2]
-    if line == "//":  # si cambiamos de PTM
-        recordno += 1  # avanzar un registro
-        record = record_cero  # y vaciar el registro
 
-    for i in range(0, len(categories)-1):
-        if line == categories[i]:
-            record[i] = line[5:]
-"""
+for gato in categories:  # para cada categoría
+    record_cero[gato] = dummy  # agregarla al registro y llenar el campo con el valor por defecto
 
-"""
-for i in range(0, 12):  # len(categories)-1
-    line = ptmlist.readline()
-    if line[:2] == categories[i]:
-        record[i] = line[5:]
-        print(record[i])
-"""
+
+record = record_cero  # defino el diccionario de registros, después me va a servir para vaciarlo
+
+line = ptmlist.readline()  # comienzo a contar lineas, asigno el valor de la primera a "line"
 i = 0
-line = ptmlist.readline().lstrip('\n')
-while i < 13:
-    print line[:2] + categories [i]
-    if line[:2] == categories[i]:  # si la linea corresponde al campo "i"a
-        print('se guarda ', line[5:], ' en ' + categories[i]) # HACER QUE IMPRIMA EN UNA LINE
-        #record[categories[i]] = line[5:]  # guardar sus datos en el campo que corresponde a la categoria
-        #print(categories[i] + ' --- ' + record[categories[i]])  # e imprimir la categoria
-        line = ptmlist.readline().lstrip('\n')
-    i += 1
+while line != '':
+    for cat in categories:  # toma cada elemento de categoria
+        if line[:2] == cat:  # y si la linea corresponde a la categoria
+            record[cat] = line[5:-1]  # agrega su contenido al registro para esa categoria
+            line = ptmlist.readline()  # y cambia a una nueva linea
+            while line[:2] == cat:  # mientras la linea nueva sea de la misma id que la anterior
+                record[cat] += '\n' + line[5:-1]  # agrega su contenido con el separador de nueva linea
+                line = ptmlist.readline()  # y cambia a una nueva linea
+    if line[:2] == '//':  # y si la nueva linea es un separador de PTMs "//"
+        # INSERT INTO ptm's VALUES record ??
+        # acá falta el query, que no se hacer todavía
+        i += 1  # vamos contando PTMs separadas por la doble barra "//"
+        for perro in record:  # un lindo print para ver que anda to-do =)
+            print(perro + ' (PTM ' + str(i) + ')' + ':\n' + record[perro])
+        record = record_cero  # después del insert, vaciar el registro
+        line = ptmlist.readline()  # y cambiar de linea
+    # si la linea está vacía, se llegó al final del archivo y el while termina
+
+# antes el registro lo escribí así
+#record_cero = {'ID': dummy, 'AC': dummy, 'FT': dummy, 'TG': dummy, 'PA': dummy,
+#    'PP': dummy, 'CF': dummy, 'MM': dummy, 'MA': dummy, 'LC': dummy, 'TR': dummy, 'KW': dummy, 'DR': dummy}"""
