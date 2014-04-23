e19aec9ed057156faa041884e5bc6a65f37fcaa3
nicolas <nico.fruta@gmail.com>
2014-04-23 13:13:11 -0300

    me cambie a mysql, no anda el insert por sintaxis??

--- insertorr/ptminsertorr.py
+++ insertorr/ptminsertorr.py
@@ -1,10 +1,15 @@
 # coding=utf-8
 __author__ = 'nicolas'
-from psycopg2 import *
-import psycopg2
-from collections import OrderedDict
+from os.path import expanduser
+from ordereddict import OrderedDict
+from sql import *
+from sql.aggregate import *
+from sql.conditionals import *
 
-ptmlist = open('../ptmlist', 'r')
+ptmlist_file = expanduser("~") + '/QB9-git/QB9/ptmlist'
+ptmlist = open(ptmlist_file)
+output_file = expanduser("~") + '/QB9-git/QB9/resources/output.txt'
+output = open(output_file, 'w')
 
 categories = OrderedDict()  # las categorías están en un diccionario con su type de postgresql, dsp los cambio
 categories['ID'] = "varchar(80) PRIMARY KEY"
@@ -21,20 +26,18 @@ categories['TR'] = "varchar(80)"
 categories['KW'] = "varchar(80)"
 categories['DR'] = "varchar(80)"
 
-conn = psycopg2.connect("dbname=test user=naikymen")
-cur = conn.cursor()
-cur.execute("SHOW server_version;")
-print cur.fetchall()
+#conn = psycopg2.connect("dbname=test user=naikymen")
+#cur = conn.cursor()
+#cur.execute("SHOW server_version;")
+#print cur.fetchall()
 
 #variables del insert
-sql_insert_values = ''
+sql_insert_values = '\''
 sql_insert_columns = ''
 
 #crear la tabla
 #table_def = str(categories.items())[2:-2].replace("'", '').replace("),", "").replace(",", "").replace(" (", ", ")
 #print("CREATE TABLE IF NOT EXISTS test.ptm_table (" + table_def + ");")
-#cur.execute("CREATE TABLE IF NOT EXISTS ptm_table (" + table_def + ");")
-
 
 empty_record = OrderedDict()  # aca voy a meter duplas "nombre de columna" + "valor de campo" de c/linea
 for gato in categories:  # para cada categoría
@@ -46,9 +49,9 @@ i = 0
 
 while line != '':  # mientras la linea no sea la "última", o sea, el fin del archivo.
     if line[:2] == '//':  # si la nueva linea es un separador de PTMs "//" hacer un INSERT
-        sql_insert_columns = str(record.keys())[1:-1].replace("'", "")
-        sql_insert_values = str(record.values())[1:-1].replace("\"", "\'")
-        cur.execute("INSERT INTO test.ptm_table VALUES (" + sql_insert_values + ");")  # PERO NO ANDA!!!!!!
+        sql_insert_columns = ', '.join(record.iterkeys())  # unir los elementos de keys con comas
+        sql_insert_values = '\'' + '\', \''.join(record.itervalues()) + '\''  # unir los elementos devalues con comas
+        output.write(("INSERT INTO TABLE mysql.ptm_table VALUES (%r)" % sql_insert_values + '\n').replace("\"", ''))  # solo para probar
         i += 1  # contar PTMs separadas por la doble barra "//".
         record = empty_record  # después del insert, vaciar el registro.
         line = ptmlist.readline()  # y cambiar de linea.
@@ -63,7 +66,10 @@ while line != '':  # mientras la linea no sea la "última", o sea, el fin del ar
 
 # antes el registro lo escribí así
 #record_cero = {'ID': dummy, 'AC': dummy, 'FT': dummy, 'TG': dummy, 'PA': dummy,
-#    'PP': dummy, 'CF': dummy, 'MM': dummy, 'MA': dummy, 'LC': dummy, 'TR': dummy, 'KW': dummy, 'DR': dummy}"""
+#    'PP': dummy, 'CF': dummy, 'MM': dummy, 'MA': dummy, 'LC': dummy, 'TR': dummy, 'KW': dummy, 'DR': dummy}
 
-cur.execute("SELECT * FROM test.ptm_table")
-cur.fetchall()
+#cur.execute("SELECT * FROM test.ptm_table")
+#cur.fetchall()
+#cur.close()
+
+# HOLA!!! """
