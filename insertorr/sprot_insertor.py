6cacf0c32f1e44bbd3db6b1c6414fc24b448a8e1
nicolas <nico.fura@gmail.com>
2014-06-19 13:11:54 -0300

    Up!

--- insertorr/sprot_insertor.py
+++ insertorr/sprot_insertor.py
@@ -14,7 +14,7 @@ output = open(output_file, 'w')
 categories = OrderedDict()
 categories['AC'] = "varchar(200) PRIMARY KEY"  # accesion number
 categories['OC'] = "varchar(200)"  # organism classification
-categories['CC'] = "varchar(200)"  # comments section, nos interesa el campo "PTM"
+# categories['CC'] = "varchar(200)"  # comments section, nos interesa el campo "PTM"
 categories['FT'] = "varchar(200)"  # ACT_SITE, MOD_RES, LIPID, CARBOHYD, DISULFID, CROSSLNK
 categories['SQ'] = "varchar(1000)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
 # The sequence counts 60 amino acids per line, in groups of 10 amino acids, beginning in position 6 of the line.
@@ -24,36 +24,49 @@ categories['SQ'] = "varchar(1000)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXX
 # MOD_RES (excluye lipidos, crosslinks y glycanos), lipidación, puente disulfuro, cross-link, glycosylation
 # todo consider PE "protein existence", KW contiene "glycoprotein" qué otros?
 # todo también dentro de FT
-print(categories.keys())
-for o in categories.iteritems():
-    print(o[0] + " " + o[1])
+# print(categories.keys())
+# for o in categories.iteritems():
+#    print(o[0] + " " + o[1])
 
 #Defino un modelo de diccionario donde cargar los valores que voy a extraer de la lista
 empty_record = OrderedDict()
 for gato in categories:  # usando las keys de categories y un valor por defecto todo vacío no es nulo ¿cómo hago?
     empty_record[gato] = 'null'
 record = empty_record  # este es el diccionario de registros vacío que voy a usar
-for o in record.iteritems():
-    print(o[0] + " " + o[1])
+#for o in record.iteritems():
+#    print(o[0] + " " + o[1])
 
 
 i = 0
 line = sprot.readline()
-print(i)
 
-# todo esto no anda
-while i <= 50:  # mientras la linea no sea la "última", o sea, el fin del archivo.
+# todo saqué el campo de comentarios porque no pude procesar el texto para evitar las 4 lineas de copyright etc...
+# la verdad debería mirar mejor los campos, cuáles están cuáles no...
+while i <= 1000:
     for cat in categories.iterkeys():
-        if line[:2] == cat:
-            record[cat] = line[5:-1]
-            line = sprot.readline()
-            while line[:2] == cat:  # mientras la linea nueva sea de la misma id que la anterior
-                record[cat] += ' --- ' + line[5:-1]  # agrega su contenido con un separador
-                line = sprot.readline()  # y cambia a una nueva linea
-    if line[:2] == '//':  # si la nueva linea es un separador de PTMs "//" hacer un INSERT
-        print(record.items)
+        if line[:2] == cat:  # si la linea es de la categoria evaluada
+            if cat == "SQ":  # si esa categoria es la de la secuencia
+                record[cat] = line[0:-1] + " -_- "  # guardarla primera linea, agergar el separador
+                line = sprot.readline()
+                while line[:2] == "  ":  # y mientras no se especifique el campo, pasa con los de la secuencia
+                    record[cat] += " " + line[5:-1]  # grabar la linea de secuencia en el campo SQ
+                    line = sprot.readline()
+            if line[:2] == cat:
+                record[cat] = line[5:-1]
+                line = sprot.readline()
+                while line[:2] == cat:
+                    record[cat] += ' -_- ' + line[5:-1]
+                    line = sprot.readline()
+    if line[:2] == '//':
+        print(record)
         record = empty_record
-    print(i)
+    line = sprot.readline()
     i += 1
 
+"""
+i = 0
+while i <= 200:
+    line = sprot.readline()
+    print(line[:-1])
+    i += 1
 # """
