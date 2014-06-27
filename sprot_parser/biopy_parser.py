2c4265f7dc4ad5819cd12cf1c67503a3c2fdd271
nicolas <nico.fura@gmail.com>
2014-06-27 13:04:57 -0300

    Up! Biopy is awesome asdasfsdgd

--- sprot_parser/biopy_parser.py
+++ sprot_parser/biopy_parser.py
@@ -6,55 +6,65 @@ from ordereddict import OrderedDict
 from Bio import SwissProt
 
 sprot_file = expanduser("~") + '/QB9_Files/uniprot_sprot.dat'
-sprot = open(sprot_file)
 output_file = expanduser("~") + '/QB9-git/QB9/resources/output.txt'
-output = open(output_file, 'w')
 
+#  Counter
 i = 0
+#  Container for the list of features
 ftlist = []
+#  Interesting feature types
 ptmrecords = ["MOD_RES", "LIPID", "CARBOHYD", "DISULFID", "CROSSLNK"]
-
-
-"""for record in SwissProt.parse(sprot):
-    i += 1
-    print(record.sequence)
-    for a in record.features:
-        print(a)
-    if i > 1000:
-        break
-"""
-for record in SwissProt.parse(sprot):
-    i += 1
-    print("\n" + str(record.sequence_length))
-    ftlist = record.features
-    for feature in ftlist:
-        for ptmrecord in ptmrecords:
-            if feature[0] in ptmrecord:
-                print(feature)
-                print("\n" + ptmrecord)
-                print("Between " + str(feature[1]) + " and " + str(feature[2]))
-                print(feature[3])
-
-    if i > 1000:
-        break
-"""
-print(ftlist)
-print(ftlist[0])
-print(ftlist[0][0])
-i = 0
-print("\nhola \n")
-for feature in ftlist:
-    if feature[0] in ptmrecords:
-        print(feature[0])
-
-
+#  Non-experimental qualifiers for feature annotations
+neqs = ["Probable", "Potential", "By similarity"]
 #Las categorías están en un diccionario con su type de postgresql todo optimizar los campos
 categories = OrderedDict()
 categories['AC'] = "varchar(200) PRIMARY KEY"  # accesion number
 categories['OC'] = "varchar(200)"  # organism classification
+categories['OX'] = "varchar(200)"  # organism classification
 # categories['CC'] = "varchar(200)"  # comments section, nos interesa el campo "PTM"
 categories['FT'] = "varchar(200)"  # ACT_SITE, MOD_RES, LIPID, CARBOHYD, DISULFID, CROSSLNK
-categories['SQ'] = "varchar(1000)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
+categories['SQ'] = "varchar(200)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
+categories['SQl'] = "varchar(200)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
+categories['SQi'] = "varchar(200)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
+
+
+"""
+with open(sprot_file) as sprot:
+    for record in SwissProt.parse(sprot):
+        i += 1
+        print(record.sequence)
+        for a in record.features:
+            print(a)
+        if i > 1000:
+            break
+"""
+
+with open(sprot_file) as sprot:  # esto me abre y cierra el archivo al final
+    for record in SwissProt.parse(sprot):
+        i += 1
+        AC = record.accessions
+        print("\nhola " + str(AC))
+        OC = record.organism_classification
+        OX = record.taxonomy_id
+        FT = record.features
+        print("\n" + str(FT))
+        for feature in FT:
+            for tipo in ptmrecords:  # iterear las tipos interesantes
+                if feature[0] in tipo:  # si el feature evaluado interesante, imprimir las cosas de ese feature
+                    print("--- " + tipo)
+                    print("Between " + str(feature[1]) + " and " + str(feature[2]))
+                    print(feature[3])
+        SQ = record.sequence
+        print(SQ)
+        SQl = record.sequence_length
+        print("Sequence length " + str(SQl))
+        SQi = record.seqinfo
+
+        if i > 1000:
+            break
+
+
+
 # The sequence counts 60 amino acids per line, in groups of 10 amino acids, beginning in position 6 of the line.
 # http://www.uniprot.org/manual/
 # General Annotation: cofactores, mass spectrometry data, PTM (complementario al MOD_RES y otras PTMs...)
