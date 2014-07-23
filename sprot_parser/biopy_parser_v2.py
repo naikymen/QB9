ee2ea3716438ccf0476d50c72bbc15962323dafa
nicolas <nico.fruta@gmail.com>
2014-07-23 17:04:08 -0300

    WOOOO! ERRORES =D debugmodeon

--- sprot_parser/biopy_parser_v2.py
+++ sprot_parser/biopy_parser_v2.py
@@ -30,14 +30,16 @@ neqs = ["Probable", "Potential", "By similarity"]  # ¿Y "definitive"?
 # Las categorías están en un diccionario con su type de postgresql todo optimizar los campos
 categories = OrderedDict()
 categories['AC'] = "varchar(30) NOT NULL"  # accesion number
-categories['FT'] = "varchar(30)"
-categories['STATUS'] = "varchar(30)"
-categories['PTM'] = "varchar(100)"
-categories['SQ'] = "text(45000)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
-categories['LENGTH'] = "varchar(200)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
-categories['ORG'] = "text(500)"  # organism
-categories['OC'] = "varchar(30)"  # organism classification, vamos solo con el dominio
-categories['OX'] = "varchar(200)"  # taxonomic ID
+categories['FT'] = "varchar(30) NOT NULL"
+categories['STATUS'] = "varchar(30) NOT NULL"
+categories['PTM'] = "varchar(100) NOT NULL"
+categories['FROM_RES'] = "varchar(200) NOT NULL"
+categories['TO_RES'] = "varchar(200) NOT NULL"
+categories['SQ'] = "text(45000) NOT NULL"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
+categories['LENGTH'] = "varchar(200) NOT NULL"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
+categories['ORG'] = "text(500) NOT NULL"  # organism
+categories['OC'] = "varchar(30) NOT NULL"  # organism classification, vamos solo con el dominio
+categories['OX'] = "varchar(200) NOT NULL"  # taxonomic ID
 categories['HO'] = "text(500)"  # host organism
 #categories['CC'] = "varchar(200)"  # comments section, nos interesa el campo "PTM"
 #categories['SQi'] = "varchar(200)"  # SQ   SEQUENCE XXXX AA; XXXXX MW; XXXXXXXXXXXXXXXX CRC64;
@@ -55,10 +57,10 @@ for cat, value in categories.items():  # concatenaciones key y valor
 table_def = ', '.join(table_def_items)  # definicion de la tabla
 output = open(output_file, 'w')
 output.write("CREATE TABLE IF NOT EXISTS ptm_table (" + table_def + ") ENGINE=InnoDB; \n")  # guardar el CREATE en output
-#cur.execute("CREATE TABLE IF NOT EXISTS sprot2 (" + table_def + ") ENGINE=InnoDB")
-#con.commit()
+cur.execute("CREATE TABLE IF NOT EXISTS sprot2 (" + table_def + ") ENGINE=InnoDB")
+con.commit()
 
-status = "Experimental"
+data['STATUS'] = "Experimental"
 ptm = ''
 out = []
 listap = []
@@ -72,14 +74,14 @@ with open(sprot_file) as sprot:  # esto me abre y cierra el archivo al final
             out.append(features[a][0])
         interes = list(set(out).intersection(ptmrecords))  # armar un set con los interesantes y hacerlo lista interes
         if interes:  # si interes no está vacía, entonces hay algo para cargar
-            data['AC'] = record.accessions[0]  # solo el principal
-            # todo insertar el primero y la secuencia+info en una tabla
+            # Acá cargo los datos generales para las PTMs de una proteína/entrada de uniprot (instancias de entradas)
+            data['AC'] = record.accessions[0]  # solo el principal, el resto nose.
             data['SQ'] = record.sequence
             data['LENGTH'] = record.sequence_length
             #data['SQi'] = record.seqinfo
             data['OX'] = record.taxonomy_id
-            data['ORG'] = record.organism  # esto el bicho
-            data['OC'] = record.organism_classification[0]  # esto me tira el dominio del bicho
+            data['ORG'] = record.organism  # el bicho
+            data['OC'] = record.organism_classification[0]  # el dominio del bicho
             if not record.host_organism:
                 data['HO'] = 'No host'
             else:
@@ -88,7 +90,9 @@ with open(sprot_file) as sprot:  # esto me abre y cierra el archivo al final
                     olista.append((o.split(";"))[0])
                 data['HO'] = ', '.join(olista)  # y esto el host del virus ¿o parásito?
 
-        # todo cargar secuencias y ptms por separado? evitar duplicados de secuencia, relacion via AC?
+            # todo evitar duplicados de secuencia, relacion via AC?
+
+            # ahora cargo cada PTM en data (subinstancias de entrada)
             for feature in features:  # iterar los features de la entrada
                 if feature[0] in interes:  # si el titulo del FT interesa, proseguir ¡mejora un poco! =D
                     for tipo in interes:  # iterear los tipos interesantes encontrados en el feature
@@ -97,12 +101,21 @@ with open(sprot_file) as sprot:  # esto me abre y cierra el archivo al final
                             B = feature[2]  # hacia el otro. OJO hay algunos desconocidos indicados con un "?"
                             C = feature[3]  # este tiene la posta?
                             D = feature[4]  # este aparece a veces? todo wtf?
-                            status = "Experimental"
-                            ptm = ''
 
+                            # reiniciar FT, FROM y TO
+                            data['FT'] = empty_data['FT']
+                            data['FROM_RES'] = empty_data['FROM_RES']
+                            data['TO_RES'] = empty_data['TO_RES']
                             # Asignar FT
                             data['FT'] = feature[0]
+                            data['FROM_RES'] = A
+                            data['TO_RES'] = B
+                            # reiniciar
 
+
+                            # reiniciar PTM y STATUS
+                            data['PTM'] = empty_data['PTM']
+                            data['STATUS'] = "Experimental"
                             # Asignar STATUS y PTM
                             if C:  # si C (el que tiene el nombre de la PTM y el STATUS) contiene algo
                                 for neq in neqs:  # iterar los STATUS posibles
@@ -149,14 +162,15 @@ with open(sprot_file) as sprot:  # esto me abre y cierra el archivo al final
                             cur.execute(("INSERT INTO sprot2 VALUES (%r);"
                                          % sql_insert_values + '\n').replace("\"", '').replace('.', ''))
                             con.commit()
+
+                            ptm = ''
                             listap = []
                             # unir los elementos de values con comas
-        out = []
 
-        if i >= 10000:  # el número de entradas (separadas por //) es 542782 verificado con biopython
+        #if i >= 10000:  # el número de entradas (separadas por //) es 542782 verificado con biopython
         #    print("\n")
         #    print(i)
-            break
+        #    break
 # The sequence counts 60 amino acids per line, in groups of 10 amino acids, beginning in position 6 of the line.
 # http://www.uniprot.org/manual/
 # General Annotation: cofactores, mass spectrometry data, PTM (complementario al MOD_RES y otras PTMs..?)
