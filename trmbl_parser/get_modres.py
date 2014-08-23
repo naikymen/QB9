94ad5bc74058b891d62cbf1fdc2ee18c81e8f11f
nicolas <nico.fruta@gmail.com>
2014-08-23 11:33:07 -0300

    queries a s2_ptms

---
+++ trmbl_parser/get_modres.py
@@ -0,0 +1,18 @@
+__author__ = 'nicolas'
+# coding=utf-8
+
+# quiero tomar cada mod res y de su posición en la secuencia, sacar qué residuo es
+
+# Obj
+# 1) parsear rows con FT interesante
+# 2) tomar el PTM, FROM_RES, TO_RES y la SQ
+# 3) Si son iguales, se debe generar una entrada (caso CARBOHYD, LIPID...)
+# 4) Si son diferentes, se debe generar una entrada para cada uno (caso CROSSLNK, DISULFID)
+
+# Métodos
+# armar una tabla solo con las proteínas con FT ?
+# parsear de a una row ?
+# procesarla e insertarla en otra tabla ?
+# armar una función de SQL que haga esto por mí ? o en python?
+
+# usar sprot2 como tabla de prueba
