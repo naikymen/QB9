75803091e38b0eeb80633f7fe051e4449add4b16
nicolas <nico.fruta@gmail.com>
2014-12-18 17:42:01 -0300

    uniprot_parser_v02 no sequence, no residue counts

--- uniprot_parser/uniprot_parser_v02.py
+++ uniprot_parser/uniprot_parser_v02.py
@@ -15,8 +15,8 @@ database = "ptmdb"
 tabla_cuentas = "sprot_count2"
 tabla_ptms = "sprot_ptms2"
 file_name = "uniprot_sprot.dat"
-desde = 195600
-hasta = 196000  # Hay 542782 entradas de AC??
+desde = 0
+hasta = 542785  # Hay 542782 entradas de AC??
 
 # Conectar a la base de datos
 con = mdb.connect('localhost', 'nicolas', passwd="nicolaslfp", db=database)
@@ -206,28 +206,13 @@ with open(uniprot_file) as uniprot:  # esto me abre y cierra el archivo al final
                             sql_insert_values_p = '\'' + \
                                                   '\', \''.join(listap) + \
                                                   '\''
-                            """
-                            if (type(A) is str) or (type(B) is str):
-                                print(A,B)
-                                print(feature)
-                                print(i)
-                                print(sql_insert_values_p + '\n')
-
-                            if D != '':
-                                print(A,B)
-                                print(feature)
-                                print(i)
-                                print(sql_insert_values_p + '\n')
-                            """
 
                             # Que después uno como van en el INSERT
                             # El insert, en el que reemplazo ' por '', para escaparlas en sql
 
-                            if i >= desde:  # para hacerlo en partes
-                                """
-                                cur.execute(("INSERT INTO " + tabla_ptms + " VALUES (%r);"
-                                             % sql_insert_values_p).replace("-...", "").replace("\"", '').replace('.', ''))
-                                """
+                            cur.execute(("INSERT INTO " + tabla_ptms + " VALUES (%r);"
+                                         % sql_insert_values_p).replace("-...", "").replace("\"", '').replace('.', ''))
+
                                 # print("commit;")
                                 # con.commit()
 
