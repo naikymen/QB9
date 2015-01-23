eca65acf8e12a193690aa89a7bfee595037927c9
nicolas <nico.fruta@gmail.com>
2015-01-23 17:50:24 -0300

    /queries

--- results/mySQL/Queries/01_List_GroupByThings_01.sql
+++ results/mySQL/Queries/01_List_GroupByThings_01.sql
@@ -2,11 +2,11 @@
 #select ptm, count(AC) count, FROM_AA, TO_AA, ft from sprot_ptms1 where ft not like "NOFT" and TO_AA not like FROM_AA group by PTM, FROM_AA, TO_AA order by count desc
 #into outfile "/tmp/mySQL/01_List_GroupByThings_01.csv" FIELDS TERMINATED BY '&' ENCLOSED BY '%'LINES TERMINATED BY '\n';
 
-select ptm, count(AC) count, FROM_AA, TO_AA, from_res, to_res, ft from sprot_ptms1 where ft not like "NOFT" and from_res not like to_res group by PTM, FROM_AA order by count desc;
+#select ptm, count(AC) count, FROM_AA, TO_AA, from_res, to_res, ft from sprot_ptms1 where ft not like "NOFT" and from_res not like to_res group by PTM, FROM_AA order by count desc;
 #into outfile "/tmp/mySQL/01_List_GroupByThings_00.csv" FIELDS TERMINATED BY '&' ENCLOSED BY '%'LINES TERMINATED BY '\n';
-desc sprot_ptms1;
-select ptm, count(AC) count, FROM_AA, TO_AA, ft from sprot_ptms1 where ft not like "NOFT" group by PTM, FROM_AA order by count desc;
-#into outfile "/tmp/mySQL/01_List_GroupByThings_02.csv" FIELDS TERMINATED BY '&' ENCLOSED BY '%'LINES TERMINATED BY '\n';
+
+select ptm, count(AC) count, FROM_AA, TO_AA, ft from sprot_ptms2 where ft not like "NOFT" group by PTM, FROM_AA order by count desc
+into outfile "/tmp/mySQL/03_List_GroupByThings_sprot_ptms2.csv" FIELDS TERMINATED BY '&' ENCLOSED BY '%'LINES TERMINATED BY '\n';
 
 #traer las ptm's que tienen un target no identificado ('?')
 #select distinct(ptm) from sprot_ptms1 where (from_res like '?' or to_res like '?') and ft not like 'NOFT';
--- results/mySQL/Queries/03_ptm_notes_01.sql
+++ results/mySQL/Queries/03_ptm_notes_01.sql
@@ -1,2 +1,3 @@
 #SELECT ptm, inumber FROM ptmdb.sprot_ptms1 where ft not like 'NOFT' and ptm like '%methioninium%';
-select note, count(*) from sprot_ptms2 where note not like "%CAR%" group by note;
+select ac, ptm, note, ft, count(*) from sprot_ptms2 group by note;
+select distinct ptm from sprot_ptms2 where note like "CAR%";
